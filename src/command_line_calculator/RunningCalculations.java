fb947930cd788e89d9770c7fbd1a0a0b9e4108df
Adam Patterson <awpatterson217@gmail.com>
2017-03-30 17:32:14 -0500

    Update RunningCalculations.java

--- src/command_line_calculator/RunningCalculations.java
+++ src/command_line_calculator/RunningCalculations.java
@@ -3,12 +3,10 @@
  * The firstCalc method is only used once, and the nextCalc method can be run infinitely.
  * The looping of the nextCalc method allows for a more flexible user interface.
  */
-
 package command_line_calculator;
 
 public class RunningCalculations {
 	public static double currentAnswer;
-
 	// Takes in three variables (firstNum, calculation choice, NextNum). 
 	protected void firstCalc(double firstNum, String theOperator, double nextNum){
 		switch(theOperator){
@@ -33,34 +31,26 @@ public class RunningCalculations {
 		default:
 			System.out.println("ERROR: Invalid Operator");
 		} 
-	} // End firstCalc.
-	
+	} 
 	// Takes in two variables (calculation choice, nextNum).
 	protected void nextCalc(String theOperator, double nextNum){
 		switch(theOperator){
 		case "+":
 			currentAnswer = currentAnswer + nextNum;
-		
 			break;
 		case "-":
 			currentAnswer = currentAnswer - nextNum;
-
 			break;
 		case "*":
 			currentAnswer = currentAnswer * nextNum;
-			
 			break;
 		case "/":
 			if(currentAnswer > 0){currentAnswer = currentAnswer / nextNum;}
-			
 			else{System.out.println("ERROR: Division by zero");}
-	
 			break;
 		default:
 			System.out.println("ERROR: Invalid Operator");
 		} 
-	} // End nextCalc.
-
-
+	} 
 	double getCurrentAnswer() {return currentAnswer;} 
-}// End RunningCalculations class
+}
