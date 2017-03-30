06a7248c771d0c126155421185704603f85684b5
Adam Patterson <awpatterson217@gmail.com>
2017-03-30 17:32:45 -0500

    Update Test.java

--- src/command_line_calculator/Test.java
+++ src/command_line_calculator/Test.java
@@ -2,23 +2,17 @@
  *  A test class so that the code can be ran on the CLI. 
  *  This class contains the main method.
  */
-
 package command_line_calculator;
-
 public class Test {
 	public static void main(String[] args){
 		boolean calcOn = true;
-		
 		// Initializes the program and executes the selected operation on firstNum and nextNum.
 		// The answer is stored privately in the RunningCalculations class, in a private variable called currentAnswer.
 		Methods.startCalc();
-		
 		// This application loops endlessly.
 		while(calcOn){
-			
 		// keepGoing() executes the selected operation on currentAnswer and nextNum.	  
 	    Methods.keepGoing();	
 		}
-		
-	} // End main method.
-} // End Test class.
+	} 
+} 
