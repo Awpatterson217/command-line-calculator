f8a179c1b23649e8ebd4f93faaea1d26ec1ad8d4
Adam Patterson <awpatterson217@gmail.com>
2017-03-30 17:31:28 -0500

    Update Methods.java

--- src/command_line_calculator/Methods.java
+++ src/command_line_calculator/Methods.java
@@ -2,22 +2,18 @@
  *  The Methods class contains the two static methods, startCalc() and nextCalc(). 
  *  
  */
-
 package command_line_calculator;
 
 import java.util.Scanner;
 
 public class Methods {
 	static Scanner userInput = new Scanner(System.in);
-	
 	// The startCalc method is the first method executed.
 	static void startCalc(){
-		
 		// An instance of the RunningCalculation class.
 		RunningCalculations runningAnswer = new RunningCalculations();
 		
-		
-		// A simple menu which presents the user with a key, each operator is associated with a number.
+		// A simple menu which presents the user with a key.
 		// More operators can be added.
 		System.out.println("Operators");
 		System.out.println("+: Plus");
@@ -45,23 +41,17 @@ public class Methods {
 		runningAnswer.firstCalc(num1, firstOperator, num2);
 		System.out.print("Current answer: ");
 		System.out.println(runningAnswer.getCurrentAnswer());
-		
-		
-	} // End startCalc.
-	
+	} 
 	// The keepGoing method is the second and last method executed.
 	// This method can be looped endlessly.
 	static void keepGoing(){
-	
-		
 		// Creates an instance of the RunningCalculations class.
 		RunningCalculations runningAnswer = new RunningCalculations();
-		
 		// Allows the user to perform another operation on a new number(nextNum) and currentAnswer.
 		System.out.println("Next operator: ");
 		String nextOperator = userInput.next();
 		System.out.print("\n");
-
+		
 		// Allows the user to enter the next number.
 		System.out.println("Next number: ");
 		double nextNumber = userInput.nextDouble();
@@ -71,8 +61,5 @@ public class Methods {
 		runningAnswer.nextCalc(nextOperator, nextNumber);
 		System.out.print("Current Answer: ");
 		System.out.println(runningAnswer.getCurrentAnswer());
-		
-	
-	} // End keepGoing.
-	
-} // End Methods class.
+	}
+}
